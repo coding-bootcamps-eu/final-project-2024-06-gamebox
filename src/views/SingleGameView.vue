1d500ea65bd42010ba0b4932f96b5d90665ce90d
Marco Michenfelder <mvsmarco@web.de>
2024-09-12 15:33:28 +0200

    first commit, created the SingleGameView

---
+++ src/views/SingleGameView.vue
@@ -0,0 +1,24 @@
+<template>
+  <NavBar class="navbar" />
+  <h1>Ich bin die SingleGameView</h1>
+  <div class="currentGame"></div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style scoped>
+.currentGame {
+  background-color: blue;
+  border: black 5 rem;
+  width: 100%;
+  height: 100%;
+}
+</style>
