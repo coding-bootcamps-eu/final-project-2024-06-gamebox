38eff88eb09fd271f2f3e27332baa4d0b068fbfe
volker Struck <Struckvolker@web.de>
2024-09-20 08:40:22 +0200

    first commit

--- src/stores/games.js
+++ src/stores/games.js
@@ -8,22 +8,30 @@ export const gamesStore = defineStore('games', {
         {
           id: 1,
           gamename: 'HANGMAN',
-          gamepath: 'Hangman'
+          gamepath: 'Hangman',
+          imagepath:
+            'https://images.deepai.org/art-image/af4416e5be394d568ca6cc2bd502c9ba/massive-gallows-surrounded-by-smaller-gallows.jpg'
         },
         {
           id: 2,
           gamename: 'ROCK PAPER SCISSORS',
-          gamepath: 'ScissorsGame'
+          gamepath: 'ScissorsGame',
+          imagepath:
+            'https://images.deepai.org/art-image/419435d801a64027b319d46ab46e11a4/dice-cc1697-thumb.jpg'
         },
         {
           id: 3,
           gamename: 'SNAKE',
-          gamepath: 'Snake'
+          gamepath: 'Snake',
+          imagepath:
+            'https://images.deepai.org/art-image/7a09e5a49c244b068888dd2f0e6c15ef/rock-paper-scissor-7d0ac5.jpg'
         },
         {
           id: 4,
           gamename: 'HIGHER OR LOWER',
-          gamepath: 'HigherOrLower'
+          gamepath: 'HigherOrLower',
+          imagepath:
+            'https://images.deepai.org/art-image/bde592a507f649e5b64593088d2374b2/shake-snake-thumb.jpg'
         }
       ]
     }
